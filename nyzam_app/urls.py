5ce05ca7346141b75fd1fc5a0fe1a151cd88f463
azym <azymhan2007@gmail.com>
2026-04-16 18:33:21 +0500

    create admin endpoint

--- nyzam_app/urls.py
+++ nyzam_app/urls.py
@@ -3,11 +3,32 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('attendance/', include('attendance.urls')),
+    path('create-admin/', create_admin),
+
 ]
+
+
+
+
+def create_admin(request):
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
+        return HttpResponse("Admin created")
+
+    return HttpResponse("Admin already exists")
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
